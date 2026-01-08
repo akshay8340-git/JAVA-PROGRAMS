fc8e1161ae221d1b93aed2b63b349971ea95964d
akshaybandenavaru <akshaybandenavaru83@gmail.com>
2026-01-08 10:24:30 +0530

    variables

---
+++ Var1.java
@@ -0,0 +1,29 @@
+class Var1 
+{
+	public static void main(String[] args) 
+	{
+		
+		String name;
+		byte age;
+		long phno;
+		int salary;
+		float cgpa;
+		char grade;
+		
+		name = "Akshay Bandenavaru";
+		age = 22;
+		phno = 9730010803l;
+		salary = 40000;
+		cgpa = 8.4f;
+		grade = 'z';
+		
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(phno);
+		System.out.println(salary);
+		System.out.println(cgpa);
+		System.out.println(grade);
+		
+
+	}
+}
