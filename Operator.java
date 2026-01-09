fc9d2a5c6fcd45803d562632373fc7c280b1f838
akshaybandenavaru <akshaybandenavaru83@gmail.com>
2026-01-09 23:49:00 +0530

    Operators

---
+++ Operator.java
@@ -0,0 +1,24 @@
+class Operator
+{
+
+    public static void main(String[]args)
+		
+      {
+      
+	  byte d = 10;
+	  byte e = 11;
+	  int f = d + e;
+	  System.out.println(f);
+	  
+	  
+	  int a = 2147483647;
+	  int b = 2147483647;
+	  int c = a * b;
+      System.out.println(c);
+      
+	  short g = 1000;
+	  short h = 1000;
+	  int i = g / h;	
+      }
+
+}
