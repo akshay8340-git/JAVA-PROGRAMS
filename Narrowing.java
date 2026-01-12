d6e0d9b7d29c288a6ec9ed6e14f78ea90d73a805
akshaybandenavaru <akshaybandenavaru83@gmail.com>
2026-01-12 20:37:04 +0530

    narrowing concept

---
+++ Narrowing.java
@@ -0,0 +1,82 @@
+class Narrowing 
+{
+	public static void main(String[] args) 
+	{
+		/*
+		
+		float d1 = (float)34.56;
+		System.out.println(d1); // 34.56
+		
+		long l1 = (long) 34.56;
+		System.out.println(l1); // 34
+		
+		int i1 = (int)34.56;
+		System.out.println(i1); // 34
+		
+		char c1 = (char)34.56;
+		System.out.println(c1); // "
+		
+		short s1 = (short)34.56;
+		System.out.println(s1); // 34
+		
+		byte b1 = (byte) 34.56;
+		System.out.println(b1); // 34
+		
+		
+		
+		long l1 = (long)45.54f;
+		System.out.println(l1); // 45
+		
+		int i1 = (int)45.54f;
+		System.out.println(i1); // 45
+		
+		char c1 = (char)45.54f;
+		System.out.println(c1); // -
+		
+		short s1 = (short)45.54f;
+		System.out.println(s1); // 45
+		
+		byte b1 = (byte)45.54f;
+		System.out.println(b1); // 45
+		
+	
+		
+		int i1 = (int)346345352345233l;
+        System.out.println(i1);   // -810404207 (doubt)
+		
+		char c1 = (char)346345352345233l;
+		System.out.println(c1);   // ?
+		
+		short s1 = (short)346345352345233l;
+		System.out.println(s1);   // 13969
+		
+		byte b1 = (byte)346345352345233l;
+		System.out.println(b1);   // -111
+		
+		
+	    
+		char c1 = (char)12456;
+		System.out.println(c1); // ?
+		
+		short s1 = (short)12456;
+		System.out.println(s1); //12456
+		
+		byte b1 = (byte)12456;
+		System.out.println(b1); //-88
+		
+		
+		
+		short s1 = (short)'a';
+		System.out.println(s1);  //97
+		
+		byte b1 = (short) 'a';
+		System.out.println(b1); // 97
+		
+		*/
+		
+		byte b1 = (byte)10000;
+		System.out.println(b1);
+		
+		
+	}
+}
