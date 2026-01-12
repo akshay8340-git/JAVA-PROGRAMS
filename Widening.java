40eec3e61a1613d920ca61e31973df8052e1b7e9
akshaybandenavaru <akshaybandenavaru83@gmail.com>
2026-01-12 20:35:27 +0530

    widening concept

---
+++ Widening.java
@@ -0,0 +1,77 @@
+class Widening 
+{
+	public static void main(String[] args) 
+	{
+		/*
+		byte b1 = 10;
+		System.out.println(b1); // 10
+		short s1 = b1;
+		System.out.println(s1); // 10
+		 //char c1 = s1;
+		// System.out.println(c1); // Possible Lossy Conversion Error
+		int i1 = s1;
+		System.out.println(i1); // 10
+		long l1 = i1;
+		System.out.println(l1); // 10
+		float f1 = l1;
+		System.out.println(f1); // 10.0
+		double d1 = f1;
+		System.out.println(d1); // 10.0
+		
+		
+		short s1 = 100;
+		System.out.println(s1); //100
+		// char c1 = s1;
+		//System.out.println(c1); Possible Lossy Conversion Error
+		int i1 = s1;
+		System.out.println(i1); //100
+		long l1 = i1;
+		System.out.println(l1); //100
+		float f1 = l1;
+		System.out.println(f1); //100.0
+		double d1 = f1;
+		System.out.println(d1); //100.0
+		
+	
+			
+		char c1 = 121;
+		System.out.println(c1); // prints 'y'
+		int i1 = c1;
+		System.out.println(i1); // 121
+		long l1 = i1;
+		System.out.println(l1); // 121
+		float f1 = l1;
+		System.out.println(f1); // 121.0
+		double d1 = f1;
+		System.out.println(d1); // 121.0
+		
+		
+		int i1 = 14567;
+		System.out.println(i1); // 14567
+		long l1 = i1;
+		System.out.println(l1); // 14567
+		float f1 = l1;
+		System.out.println(f1); // 14567.0
+		double d1 = f1;
+		System.out.println(d1); // 14567.0
+		
+		
+		long l1 = 10085345646464l;   // 10085345646464
+		System.out.println(l1);
+		float f1 = l1;
+		System.out.println(f1);  // 1.0085346E13
+		double d1 = f1;
+		System.out.println(d1);  // 1.008534552576E13
+		
+		
+		float f1 = 1025.56f;
+		System.out.println(f1); // 1025.56
+		double d1 = f1;
+		System.out.println(d1); // 1025.56005859375
+		
+		
+		double d1 = 34.4;
+		System.out.println(d1); // 34.4
+		*/
+	}
+}
