b74163e8ac6eac36f696154ec685d4817c801a90
akshaybandenavaru <akshaybandenavaru83@gmail.com>
2026-01-13 19:07:06 +0530

    Cycle for Byte Short and Int

---
+++ Cycle.java
@@ -0,0 +1,64 @@
+class Cycle
+{
+	public static void main(String[] args) 
+	{
+		
+		/*
+	//	***Cycle of Byte***
+		
+		byte b1 = (byte)128;  // -128
+		System.out.println(b1);
+		
+		byte b2 = (byte)200;  // -56
+		System.out.println(b2);
+		
+		byte b3 = (byte)256;  // 0 return spositive values from here
+		System.out.println(b3);
+		
+		byte b6 = (byte)259;  
+		System.out.println(b6);  //3
+		
+		byte b4 = (byte)384;
+		System.out.println(b4); // -128 returns negative values from here
+		
+		byte b5 = (byte)512;  
+		System.out.println(b5);  // 0
+		
+		
+	//	***Cycle of Short***
+		
+		
+		short s1 = (short)32768;  // -32768
+		System.out.println(s1);
+		
+		short s2 = (short)65536; // 0 returns positive value from here
+		System.out.println(s2);
+		
+		short s3 = (short)98304;
+		System.out.println(s3);  // -32768  returns negative value from here
+		
+		short s4 = (short)131072;
+		System.out.println(s4);  // 0
+		 
+		short s5 = (short)1000000000000005453l;
+		System.out.println(s5);  // 5453
+		
+		
+		
+	//	*** Cycle For Int ***
+		 
+		int i1 = (int)2147483648l;
+		System.out.println(i1);  //-2147483648
+		
+		int i2 = (int)4294967296l;
+		System.out.println(i2);  // 0 positive values from here
+		
+        int i3 = (int)6442450944l;
+		System.out.println(i3);  // -2147483648
+		
+		*/
+		
+		long l1 = (long)9223372036854775808l;
+		System.out.println(l1);	
+	}
+}
