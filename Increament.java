6df7ea5c7bfd12a6007be3501a809f4fa1094635
akshaybandenavaru <akshaybandenavaru83@gmail.com>
2026-01-17 22:30:55 +0530

    Decreament Operators

---
+++ Increament.java
@@ -0,0 +1,17 @@
+class Increament
+{
+	public static void main(String[] args) 
+	{
+		
+		int a = 100;
+		
+		
+		System.out.println(++a); //101 valued added by 1 firstly then stored
+		System.out.println(++a); // 102 valued added by 1 firstly then stored
+		System.out.println(a++); // 102 first print then add 1 value so real value is now 103
+		System.out.println(++a); //104 value added by 1 then stored value is now 104
+		System.out.println(a++); // 104 first print value then add 1 so new value is 105
+		System.out.println(++a); //106 add 1 value and store 
+		
+	}
+}
