b6660b68213e0889e7641fe94ab3b6a4c1628b64
akshaybandenavaru <akshaybandenavaru83@gmail.com>
2026-02-14 23:39:16 +0530

    Factorial number

---
+++ Fact.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class Factorial
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter NO: ");
+		int num = sc.nextInt();
+		
+		int fact = 1;
+		
+		// For Loop
+		/*for (int i = 1; i <= num ; i++ )
+		{
+			fact = fact * i;
+		}
+		System.out.println(fact);*/
+		
+		// While loop
+		
+		
+		while (num > 1)
+		{
+			fact = fact * num;
+			num --;
+		}
+		
+		System.out.println(fact);
+	}
+}
