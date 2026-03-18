a546b3d3de6fc6a86b55c682805d25f923874556
akshaybandenavaru <akshaybandenavaru83@gmail.com>
2026-03-18 22:36:34 +0530

    Patterns 6

---
+++ Pattern6.java
@@ -0,0 +1,92 @@
+class Pattern6 
+{
+	public static void main(String[] args) 
+	{
+		int n = 5;
+		
+	/*	for (int i = 0; i< n ; i++ )
+		{
+			for (int sp = 0; sp < n-1-i ; sp++ )
+			{
+				System.out.print("  ");
+			}
+			
+			for (int st = 0; st < i*2-1 ;st++ )
+			{
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+		}
+		*/
+		
+		
+	/*	for (int i = 0; i < n ; i++ )
+		{
+			for (int sp = 0; sp < n - 1 - i ; sp++ )
+			{
+				System.out.print("  ");
+			}
+			for (int st = 0; st < n ; st++ )
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		
+		*/
+		
+		
+	/*	for (int i = 0; i < n ; i++ )
+		{
+			for (int sp = 0; sp < i ; sp++ )
+			{
+				System.out.print("  ");
+			}
+			
+			for (int st = 0; st < n ;st++ )
+			{
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+		}
+		*/
+		
+	/*	for (int i = 0; i< n ; i++ )
+		{
+			for (int sp = 0; sp < i ; sp++ )
+			{
+				System.out.print("  ");
+			}
+			
+			for (int st = 0; st < (2*n)-(2*i + 1	) ;st++ )
+			{
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+		}
+		
+		*/
+		
+	/*	for (int i = 0; i < n ; i++ )
+		{
+			for (int sp = 0; sp < n - i ;sp++ )
+			{
+				System.out.print("  ");
+			}
+			
+			for (int st = 0; st <= i ;st++ )
+			{
+				System.out.print("*   ");
+			}
+			System.out.println();
+		}
+		
+		*/
+		
+		
+		
+	}
+}
