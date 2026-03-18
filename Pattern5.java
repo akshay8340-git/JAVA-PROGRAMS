3e6bc24bb5734a1d34691a44a889a33cd3a9aef8
akshaybandenavaru <akshaybandenavaru83@gmail.com>
2026-03-18 22:35:48 +0530

    Pattern5

---
+++ Pattern5.java
@@ -0,0 +1,69 @@
+class Pattern5
+{
+	public static void main(String[] args) 
+	{
+		int n = 5;
+		/*
+		for (int i = 0; i< n ;i++ )
+		{
+			for (int j = 0; j < n ; j++ )
+			{
+				if (j < i)
+				{
+					System.out.print("  ");
+				}
+				else
+				{
+					System.out.print("* ");
+				}
+			}
+			
+			System.out.println();
+		}
+		*/
+		
+		/*
+		System.out.println();
+		
+		for (int k = 0; k < n ; k++ )
+		{
+			for (int l = 0;l < n ; l++ )
+			{
+				if (l < n - 1 - k)
+				{
+					System.out.print("  ");
+				}
+				else
+				{
+					System.out.print("* ");
+				}
+			}
+			System.out.println();
+		}
+		*/
+		
+		/*
+		System.out.println();
+		
+		for (int m = 0; m < n ; m++)
+		{
+			for (int o = 0;o <= m ; o++ )
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	 	*/
+		
+		/*
+		for (int i = 0; i < n ; i++ )
+		{
+			for (int j = 0; j < n - i ; j++ )
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		*/
+	}
+}
